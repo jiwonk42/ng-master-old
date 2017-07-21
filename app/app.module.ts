78bd18b964259996a64122265e88d3067688ee64
Jiwon Kang <jiwonk42@gmail.com>
2017-07-20 18:23:42 -0700

    make the form work

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { YTApiModule } from 'ngx-youtube';
-
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { YTApiModule } from 'ngx-youtube';
 import { AppComponent } from './app.component';
 import { VideosComponent } from './videos/videos.component';
 import { routing } from './app.routing';
@@ -20,15 +19,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
-    routing,
-    YTApiModule.setConfig(
-      {
-        clientId: "549414897645-dupdge5q3kuuq798o4banq84gvovrjpg.apps.googleusercontent.com",
-        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"],
-        scope: "https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner"
-      }
-    )
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app/videos/videos.component.ts
+++ app/videos/videos.component.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {YTApiService} from 'ngx-youtube';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-videos',
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.css']
 })
-export class VideosComponent implements OnInit {
+export class VideosComponent {
 
-  constructor(private router: Router, private YTS: YTApiService){}
+  //searchForm: any;
+  searchForm: FormGroup;
+  search = new FormControl ("", Validators.required);
+  results$: any;
 
-  ngOnInit() {
-  }
-  doSearch(keyword){
-    let request = this.YTS.search(keyword, 25, 'video');
-    request.execute(res=>{
-        console.log(res);
+  constructor(private router: Router, private _formBuilder: FormBuilder, private _http: Http) {
+    const API_URL = 'https://www.googleapis.com/youtube/v3/search';
+    // get a key at https://console.developers.google.com/apis/api/youtube
+    const API_KEY = 'AIzaSyABuOWB8fp2wNBpNPHZCqeJtSo637MM5V8';
+
+    this.searchForm = this._formBuilder.group({
+      'search': ['', Validators.required]
     });
-  }
 
-  doLogin(ev){
-    console.log(this.YTS.auth(ev));
+    this.results$ = this.searchForm.valueChanges.subscribe(value => console.log(value));
+    /*this.results$ = this.searchForm.controls.search.valueChanges // <- Observable Form
+      .subscribe(value => console.log(value));*/
+
+      /*.switchMap(query => this._http.get(`${API_URL}?q=${query}&key=${API_KEY}&part=snippet`))  // <-- Observable Http
+      .map(res => res.json())
+      .map(res => res.items);
+    console.log(this.results$);*/
   }
 }
