6671618972cf8c4ed4b8c40728adb0cdacbaa022
Jiwon Kang <jiwonk42@gmail.com>
2017-07-25 18:46:44 -0700

    add a Carousel to Home Component

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  fullImagePath1: string;
+  fullImagePath2: string;
+  fullImagePath3: string;
 
-  constructor() { }
+  constructor() {
+    this.fullImagePath1 = '/assets/images/gummy.jpg';
+    this.fullImagePath2 = '/assets/images/gummy2.jpg';
+    this.fullImagePath3 = '/assets/images/gummy3.jpg';
+  }
 
   ngOnInit() {
   }
