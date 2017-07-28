fce534c89d27233246671ade9ee7435c2aecbba5
Jiwon Kang <jiwonk42@gmail.com>
2017-07-28 09:24:11 -0700

    add three small paragraph section to Home Component

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -10,12 +10,18 @@ export class HomeComponent implements OnInit {
   fullImagePath2: string;
   fullImagePath3: string;
   fullImagePath4: string;
+  fullImagePath5: string;
+  fullImagePath6: string;
+  fullImagePath7: string;
 
   constructor() {
     this.fullImagePath1 = '/assets/images/homepage.jpeg';
-    this.fullImagePath2 = '/assets/images/angular-cli.jpg';
-    this.fullImagePath3 = '/assets/images/typescript.png';
+    this.fullImagePath2 = '/assets/images/angular-cli.png';
+    this.fullImagePath3 = '/assets/images/javascript.png';
     this.fullImagePath4 = '/assets/images/angular-4.png';
+    this.fullImagePath5 = '/assets/images/goal.jpg';
+    this.fullImagePath6 = '/assets/images/rocket.png';
+    this.fullImagePath7 = '/assets/images/development.png';
   }
 
   ngOnInit() {
