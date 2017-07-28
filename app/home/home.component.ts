102dafe91b667c78645a9872dd55667bbffea59d
Jiwon Kang <jiwonk42@gmail.com>
2017-07-28 08:11:54 -0700

    add main paragraph to Home Component

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -9,11 +9,13 @@ export class HomeComponent implements OnInit {
   fullImagePath1: string;
   fullImagePath2: string;
   fullImagePath3: string;
+  fullImagePath4: string;
 
   constructor() {
     this.fullImagePath1 = '/assets/images/homepage.jpeg';
     this.fullImagePath2 = '/assets/images/angular-cli.jpg';
     this.fullImagePath3 = '/assets/images/typescript.png';
+    this.fullImagePath4 = '/assets/images/angular-4.png';
   }
 
   ngOnInit() {
